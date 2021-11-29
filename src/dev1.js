976e1e26f3615f86ce3f4fe067237ddf2923ebe5
Soft-Designs <Soft-Designs@users.noreply.github.com>
2021-11-28 22:59:54 -0500

    2021.11.28.CT-23:00 - Setup JSDoc - Init Repo

---
+++ src/dev1.js
@@ -0,0 +1,13 @@
+/**
+ * Represents a book.
+ * @constructor
+ * @param {string} title - The title of the book.
+ * @param {string} author - The author of the book.
+ */
+ function Book(title, author) {
+    let errTest = title + 25;
+    console.log(errTest);
+  }
+  
+  let x = new Book("IronJohn", "RobertBly");
+  
