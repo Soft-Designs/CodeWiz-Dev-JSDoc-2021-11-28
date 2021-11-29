7aa165de1cdea48096272465d669eb337c62a0cb
Soft-Designs <Soft-Designs@users.noreply.github.com>
2021-11-28 23:30:22 -0500

    2021.11.28.CT-23:30 - Stable

--- src/dev1.js
+++ src/dev1.js
@@ -4,10 +4,9 @@
  * @param {string} title - The title of the book.
  * @param {string} author - The author of the book.
  */
- function Book(title, author) {
-    let errTest = title + 25;
-    console.log(errTest);
-  }
-  
-  let x = new Book("IronJohn", "RobertBly");
-  
+function Book(title, author) {
+  let errTest = title + 25;
+  console.log(errTest);
+}
+
+let x = new Book("IronJohn", "RobertBly");
